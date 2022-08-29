6a205a65eb01dabeb2addf6151c7a92cd6ca317e
tanmayhedau <tanmayhedau619@gmail.com>
2022-08-29 22:22:52 +0530

    29aug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://tannmayhedau619:Tanmay%40619@cluster0.fw1xhuw.mongodb.net/tanmayhedau619-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,59 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
-    next()
+const authenticate = function (req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if (!token) {
+        token = req.headers["x-auth-token"];
+    } if (!token) {
+        return res.send({ status: false, data: "token is mandatory" });
+    }
+    next();
 }
 
 
-const authorise = function(req, res, next) {
+const userLogin = async function (req, res, next) {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user) {
+        return res.send({ status: false, data: "username or password is not correct" });
+    } else {
+        req.user = user;
+        next();
+    }
+
+}
+
+
+const verifyToken = function (req, res, next) {
+
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, 'functionup-plutonium');
+    if (!decodedToken) {
+        res.send({ status: false, data: "token is invalid" });
+    } else {
+            req["decodedToken"]=decodedToken
+        next();
+    }
+}
+
+
+const authorise = function (req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+    let decodedToken = req.decodedToken;
+    let userId = req.params.userId;
+    if (decodedToken.userId == userId) {
+        next();
+    } else {
+        return res.send({ status: false, data: "user does not matched" })
+    }
 }
+
+
+
+module.exports.authenticate = authenticate;
+module.exports.userLogin = userLogin;
+module.exports.verifyToken = verifyToken;
+module.exports.authorise = authorise;
