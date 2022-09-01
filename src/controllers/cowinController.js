697c387335136be880f9be5f4d515b64a66673d8
tanmayhedau <tanmayhedau619@gmail.com>
2022-09-01 17:40:50 +0530

    01sep

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -47,20 +47,21 @@ let getByPin = async function (req, res) {
             method: "get",
             url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
         }
+
         let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
+        console.log(result.data);
+        res.status(200).send({ msg: result.data });
     }
     catch (err) {
         console.log(err)
-        res.status(500).send({ msg: err.message })
+        res.status(500).send({ msg: err.message });
     }
 }
 
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -78,8 +79,28 @@ let getOtp = async function (req, res) {
     }
 }
 
+let getDistrictsId = async function (req, res) {
+    try {
+        let district = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${district} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${date}`
+        }
+        
+        let result = await axios(options)
+        console.log(result.data);
+        res.status(200).send({ msg: result.data });
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message });
+    }
+}
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getDistrictsId = getDistrictsId
--- src/routes/route.js
+++ src/routes/route.js
@@ -14,9 +14,10 @@ router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get("/cowin/getByDistrict", CowinController.getDistrictsId)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
-
+ 
 module.exports = router;
